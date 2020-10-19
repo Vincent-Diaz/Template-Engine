5197a324bd0bc91a057f010a4da24746598cef7c
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-10-18 21:38:00 -0500

    added manager properties and methods

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNum) {
+        
+        super(name, id, email);
+        this.office = officeNum;
+    }
+
+    getOffice() {
+        return this.office
+    }
+
+    getRole() {
+        return "Manager";
+    }
+}
+
+module.exports = Manager;
