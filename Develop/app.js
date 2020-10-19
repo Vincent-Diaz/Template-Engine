091aa880c3f6def5022a6f80e670c502082b523f
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-10-18 22:59:23 -0500

    added empty array added manager prompt and employee prompt

--- Develop/app.js
+++ Develop/app.js
@@ -10,10 +10,55 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+const teamList = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function addManager () {
+    inquirer.prompt ([
+        {
+            type: "input",
+            message: "What is the manager's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is the manager's id?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is the manager's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is the manager's office number?",
+            name: "officeNo"
+        },
+    ]).then(response => {
+        const manager = new Manager(response.name, response.id, response.email, response.officeNo);
+        teamList.push(manager);
+    });
+}
 
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What role would you like to add?",
+            choices: ["Engineer", "Intern"],
+            name: "role"
+        },
+    ]).then(answer => {
+        if(answer.role === "Engineer"){
+            addEngineer();
+        } else if (answer.role === "Intern"){
+            addIntern();
+        }else {
+            render(teamList);
+        }
+    })
+}
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
