474275d1060c9786091373dbaddd6feb9ecae4b9
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-10-20 15:07:20 -0500

    added util.promisify and moved writefile to line 62

--- Develop/app.js
+++ Develop/app.js
@@ -4,6 +4,9 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -56,7 +59,7 @@ function addEmployee() {
         } else if (answer.role === "Intern") {
             addIntern();
         } else {
-            render(teamList);
+            writeFileAsync(outputPath, render(teamList), "utf-8");
         }
     })
 }
@@ -113,13 +116,17 @@ function addIntern() {
             name: "school"
         },
     ]).then(data => {
-        const intern = new Engineer(data.name, data.id, data.email, data.school);
+        const intern = new Intern(data.name, data.id, data.email, data.school);
         teamList.push(intern);
         addEmployee();
     });
 }
 
-
+// function generateTeam() {
+//     writeFileAsync(outputPath, render(teamList), "utf-8");
+//     console.log("Team page generated!")
+// }
+// generateTeam();
 initApp();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
